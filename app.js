63643af9ab03225919e1f2121f9f4776ad6ffeba
Michael Staley <mstaleynet@gmail.com>
2017-08-27 02:44:05 -0500

    Initialize repo

---
+++ app.js
@@ -0,0 +1,11 @@
+var name = 'Michael';
+
+for (i=0; i<10; i++) {
+	console.log(i);
+}
+
+var sayHello = (someName) => console.log(`Hello ${name}!! This is Node!!!!!!!`);
+
+//console.log('Hello from Node!!!!!!!');
+
+sayHello(name);
